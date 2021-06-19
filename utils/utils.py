af4e8259dfcb18aa0f5ee4e9593e5f934596e5ee
Laffery <2387065420@qq.com>
2021-06-19 15:00:10 +0800

    :zap:

--- utils/utils.py
+++ utils/utils.py
@@ -2,13 +2,14 @@
 Copyrights: ©2021 @Laffery
 Date: 2021-06-09 08:28:03
 LastEditor: Laffery
-LastEditTime: 2021-06-10 19:15:08
+LastEditTime: 2021-06-19 14:59:33
 '''
 import time
 import json
 import datetime
 from io import BytesIO
 from PIL import Image
+import base64
 
 def time_stamp():
     '''
